b875812968ff62af1538aba5cc2f6cecc49f8d9e
thienlp <thienlp@dktechgroup.com>
2024-04-16 13:35:56 +0700

    double lib

--- admobxapplovin/src/main/java/com/dktlib/ironsourcelib/utils/admod/NativeHolderAdmod.kt
+++ admobxapplovin/src/main/java/com/dktlib/ironsourcelib/utils/admod/NativeHolderAdmod.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import com.applovin.mediation.MaxAd
 import com.google.android.gms.ads.nativead.NativeAd
 
-class NativeHolderAdmod(var ads: String, var ads2: String){
+class NativeHolderAdmod(var ads: String){
     var nativeAd : NativeAd?= null
     var isLoad = false
     var native_mutable: MutableLiveData<NativeAd> = MutableLiveData()
