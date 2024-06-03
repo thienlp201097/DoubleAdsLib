75893525da7d6071cf28e6c4973f3295985bf2ad
thienlp <thienlp@dktechgroup.com>
2024-06-03 11:07:34 +0700

    fix dialog onresume

--- admobxapplovin/src/main/java/com/dktlib/ironsourcelib/adjust/AdjustUtils.kt
+++ admobxapplovin/src/main/java/com/dktlib/ironsourcelib/adjust/AdjustUtils.kt
@@ -1,6 +1,7 @@
 package com.dktlib.ironsourcelib.adjust
 
 import android.content.Context
+import android.util.Log
 import com.adjust.sdk.Adjust
 import com.adjust.sdk.AdjustAdRevenue
 import com.adjust.sdk.AdjustConfig
@@ -28,9 +29,12 @@ object AdjustUtils {
         val adjustAdRevenue = AdjustAdRevenue(AdjustConfig.AD_REVENUE_APPLOVIN_MAX)
         adjustAdRevenue.setRevenue(ad.revenue, "USD")
         adjustAdRevenue.setAdRevenueNetwork(ad.networkName)
+        Log.d("==postRevenueAdjustMax==", "postRevenueAdjustMax: ${ad.networkName}")
         if (ad.networkName == "AdMob" || ad.networkName == "Google Ad Manager"){
+            Log.d("==postRevenueAdjustMax==", " ${ad.networkName} no post revenue")
             return
         }
+        Log.d("==postRevenueAdjustMax==", " ${ad.networkName} post revenue")
         adjustAdRevenue.setAdRevenueUnit(ad.adUnitId)
         adjustAdRevenue.setAdRevenuePlacement(ad.placement)
         Adjust.trackAdRevenue(adjustAdRevenue)
